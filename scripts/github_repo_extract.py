78c2583ccd58fcc27363ea50b720bbd396decee6
Dennis Irorere <35724017+denironyx@users.noreply.github.com>
2023-06-04 10:12:08 +0100

    create functions and add some checks:

--- scripts/github_repo_extract.py
+++ scripts/github_repo_extract.py
@@ -24,63 +24,96 @@ page_num = 1
 
 # Create empty list
 pages = []
-names = []
-forks = []
 
-# Waiting until things break
-while True:
-    print("------")
-    print(f"Page: {page_num}")
-    url = f"https://api.github.com/users/{user_account}/repos?page={page_num}"
-    
-    # Establish url request 
-    response = requests.get(url, headers=headers)
+# Custom Duplicate Checker
+def duplicate_checker(values):
+        def has_duplicates(values):
+            if len(values) != len(set(values)):
+                return True
+            else:
+                return False
+        if has_duplicates(values):
+            duplicate_in_coin = values.duplicated(subset=['names'])
+            if duplicate_in_coin.any():
+                values = values.loc[~duplicate_in_coin].reset_index(drop = True)
+                return values
 
-    # Code here will only run if the request is successful
-    if(response.status_code == 200):
-        print('The request was a success!')
+def scrape_github_data():
+    # Waiting until things break
+    while True:
+        print("------")
+        print(f"Page: {page_num}")
+        url = f"https://api.github.com/users/{user_account}/repos?page={page_num}"
         
-        # Decode the JSON response into aa dictionary and use the data
-        data = response.json()
+        # Establish url request 
+        response = requests.get(url, headers=headers)
+
+        # Test 1: Code here will only run if the request is successful
+        if(response.status_code == 200):
+            print('The request was a success!')
+            
+            # Decode the JSON response into aa dictionary and use the data
+            data = response.json()
+
+            #Test 2: Debug by printing out the first data extracted
+            try:
+                print(data[0]['full_name'])
+            except:
+                print("There was an error while fetching the data")
+                break
+
+        # Code here will break on failed request
+        else:
+            print('There is a failed request! ', response.status_code)
         
-        # Debug by printing out the first data extracted
-        try:
-            print(data[0]['full_name'])
-        except:
-            print("There was an error while fetching the data")
+        # if we find repos name in a page, append them to our list
+        pages.append(data)
+        
+        # Checking if we are getting data from the right source
+        if(pages[0][0]['owner']['login'].upper() == user_account.upper()):
+            print(f"Looking good! {pages[0][0]['owner']['login']}")
+        else:
+            print('Warning: we are getting data from the wrong source')
+
+        # increase the page count by 1
+        page_num = page_num + 1
+        
+    # loop pages to check for empty pages
+    for page in pages:
+        if page == []:
+            print(f"There is no repos in this page: {pages.index(page)}")
             break
 
-    # Code here will break on failed request
-    else:
-        print('There is a failed request! ', response.status_code)
+    # count the number of repos found
+    len(pages[0])
+
+    # Extract the repo names
+    repo_names = []
+    forks = []
+    for page in pages:
+        for repo in page:
+            try:
+                repo_names.append(repo['full_name'].split("/")[1])
+                forks.append(repo['forks'])
+            except:
+                pass
+
+    # create a dataframe and store the values in it
+    repo_data = pd.DataFrame()
+    repo_data['name'] = repo_names
+    repo_data['number_of_forks'] = forks
+
+    # Check for duplicate with this custom duplicate checker
+    duplicate_checker(repo_data)
     
-    # if we find repos name in a page, append them to our list
-    pages.append(data)
+    return repo_data 
 
-    # increase the page count by 1
-    page_num = page_num + 1
-    
-# loop pages to check for empty pages
-for page in pages:
-    if page == []:
-        print(f"There is no repos in this page: {pages.index(page)}")
-        break
+# Export data
+def export_to_csv(dataframe, filename):
+    dataframe.to_csv(filename, index=False)
 
-# count the number of repos found
-len(pages[0])
+# Scrape GitHub data and store it in a dataframe
+df = scrape_github_data()
 
-# Extract the repo names
-repo_names = []
-forks = []
-for page in pages:
-    for repo in page:
-        try:
-            repo_names.append(repo['full_name'].split("/")[1])
-            forks.append(repo['forks'])
-        except:
-            pass
-
-# create a dataframe and store the values in it
-repo_data = pd.DataFrame()
-repo_data['name'] = repo_names
-repo_data['number_of_forks'] = forks
+# Export the dataframe to a CSV file
+export_to_csv(df, "github_data.csv")
